226324723b524fbe78d020e1566cc2e838be85d2
Yanis Bouchenna <bouchenna.yanis@protonmail.com>
2019-12-11 18:02:00 +0100

    Finished server implementation

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import GetMusicListView, GetPlaylistsView
+from .views import GetAuthorizationURI, GetTokens, GetSongsList, GetPlaylists
 
 
 urlpatterns = [
     #path("login/", LoginView.as_view()),
-    path("music-list/", GetMusicListView.as_view()),
-    path("playlists-list/", GetPlaylistsView.as_view()),
+    path("auth-url/", GetAuthorizationURI.as_view()),
+    path("tokens/", GetTokens.as_view()),
+    path("songs-list/", GetSongsList.as_view()),
+    path("playlists/", GetPlaylists.as_view()),
 ]
--- src/api/views.py
+++ src/api/views.py
@@ -3,54 +3,82 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from collections import namedtuple
+import datetime
+import requests
+import json
+
 from gmusicapi import Mobileclient
 
-from oauth2client.client import AccessTokenCredentials, OAuth2WebServerFlow, OAuth2Credentials
+from oauth2client.client import OAuth2Credentials, OAuth2WebServerFlow
 
 # Create your views here.
-oauth = (
-        '228293309116.apps.googleusercontent.com',
-        'GL1YV0XMp0RlL7ylCV3ilFz-',
-        'https://www.googleapis.com/auth/skyjam',
-        'urn:ietf:wg:oauth:2.0:oob'
-    )
 
-class GetMusicListView(APIView):
+OAuthInfo = namedtuple('OAuthInfo', 'client_id client_secret scope redirect_uri')
+oauth = {
+        'client_id':'228293309116.apps.googleusercontent.com',
+        'client_secret':'GL1YV0XMp0RlL7ylCV3ilFz-',
+        'scope':'https://www.googleapis.com/auth/skyjam',
+        'redirect_uri':'urn:ietf:wg:oauth:2.0:oob'
+    }
+
+class GetAuthorizationURI(APIView):
     def get(self, request, *args, **kwargs):
-        token = request.POST.get('token')
-        device_id = request.POST.get('device-id') or Mobileclient.FROM_MAC_ADDRESS
+        auth_uri = "https://accounts.google.com/o/oauth2/v2/auth?client_id=228293309116.apps.googleusercontent.com&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fskyjam&access_type=offline&response_type=code"
 
+        return Response({'auth-uri':auth_uri})
 
-
-        flow = OAuth2WebServerFlow(client_id=oauth[0], client_secret=oauth[1], scope = oauth[2], redirect_uri=oauth[3])
-        credentials = OAuth2Credentials(access_token=str(token), client_id=Mobileclient.FROM_MAC_ADDRESS, client_secret=oauth[1], refresh_token = str(token), token_expiry=None, token_uri="https://accounts.google.com/o/oauth2/token", user_agent="user-agent/1.0")
-        
-
-
-
-
-
-        mobile_client = Mobileclient()
-
-        #credentials = AccessTokenCredentials(access_token = str(token), user_agent="user-agent/1.0")
-        
-        mobile_client.oauth_login(oauth_credentials=credentials, device_id=device_id)
-        return Response(mobile_client.get_all_songs())
-
-class GetPlaylistsView(APIView):
+class GetTokens(APIView):
     def get(self, request, *args, **kwargs):
+        
+        code = request.headers['auth-code']
+        print(code)
+        if not code:
+            return Response({'error':'no code'})
+        
+        flow = OAuth2WebServerFlow(client_id=oauth['client_id'], client_secret=oauth['client_secret'], scope=oauth['scope'], redirect_uri=oauth['redirect_uri'])
+        credentials = flow.step2_exchange(code)
 
-        user_email = request.POST.get("email")
-        user_password = request.POST.get("password")
-        android_id = request.POST.get("android-id")
+        return Response({'access-token':credentials.access_token, 'refresh-token':credentials.refresh_token})
 
-        if android_id == None:
-            android_id = Mobileclient.FROM_MAC_ADDRESS
+class GetSongsList(APIView):
+    def get(self, request, *args, **kwargs):
+        token = request.headers['token']
+        refresh_token = request.headers['refresh-token']
 
-        mobile_client = Mobileclient()
+        expiry_response = requests.get("https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={}".format(token))
 
-        if not mobile_client.login(email=user_email, password=user_password, android_id=android_id):
-            return Response("LOGIN FAIL")
+        remaining_time = json.loads(expiry_response.content)['expires_in']
 
-        return Response(mobile_client.get_all_playlists())
 
+        now = datetime.datetime.now()
+        token_expiry = datetime.datetime.fromtimestamp(now.timestamp() + remaining_time)
+
+        credentials = OAuth2Credentials(access_token=token, client_id=oauth['client_id'], client_secret=oauth['client_secret'], refresh_token=refresh_token, token_expiry=token_expiry,token_uri="https://www.googleapis.com/oauth2/v4/token", user_agent="user-agent/1.0")
+
+        client = Mobileclient()
+        if not client.oauth_login(device_id=Mobileclient.FROM_MAC_ADDRESS, oauth_credentials=credentials):
+            return Response({'error':'Login Failed'})
+        
+        return Response(client.get_all_songs())
+
+class GetPlaylists(APIView):
+    def get(self, request, *args, **kwargs):
+        token = request.headers['token']
+        refresh_token = request.headers['refresh-token']
+
+        expiry_response = requests.get("https://www.googleapis.com/oauth2/v1/tokeninfo?access_token={}".format(token))
+
+        remaining_time = json.loads(expiry_response.content)['expires_in']
+
+
+        now = datetime.datetime.now()
+        token_expiry = datetime.datetime.fromtimestamp(now.timestamp() + remaining_time)
+
+        credentials = OAuth2Credentials(access_token=token, client_id=oauth['client_id'], client_secret=oauth['client_secret'], refresh_token=refresh_token, token_expiry=token_expiry,token_uri="https://www.googleapis.com/oauth2/v4/token", user_agent="user-agent/1.0")
+
+        client = Mobileclient()
+        if not client.oauth_login(device_id=Mobileclient.FROM_MAC_ADDRESS, oauth_credentials=credentials):
+            return Response({'error':'Login Failed'})
+        
+        return Response(client.get_all_playlists())
